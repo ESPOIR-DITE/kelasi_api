ca36fe5e6ad2442e756619c68a5d570ea9f8197b
Espoir <216093805@mycput.ac.za>
2021-02-04 12:32:07 +0200

    Ready to be deployed for testing.

--- src/main/java/kelasi/ac/za/kelasi_api/Configuration/SecurityConfiguration.java
+++ src/main/java/kelasi/ac/za/kelasi_api/Configuration/SecurityConfiguration.java
@@ -1,34 +1,41 @@
-package kelasi.ac.za.kelasi_api.Configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    String Role ="USER";
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles(Role)
-                        .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeRequests()
-                .antMatchers("kelasi/**").hasRole(Role)
-                .anyRequest().authenticated()
-                .and();
-    }
-}
+//package kelasi.ac.za.kelasi_api.Configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    String Role ="USER";
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("1234")
+//                        .roles(Role)
+//                        .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .authorizeRequests()
+//                .antMatchers("/**").hasRole(Role)
+//                .anyRequest().authenticated()
+//                .and();
+////                .formLogin()
+////                .loginPage("/login")
+////                .permitAll()
+////                .and();
+//    }
+//}
